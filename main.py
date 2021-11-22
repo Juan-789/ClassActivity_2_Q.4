a0936393001f4c4b58d7180166178b5778646b08
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-11-22 11:25:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 """
 Write a program that asks a user for a two digit integer. Swap its digits and print the output.
 """
-def twoDigs():
-  digs = int(input("what numbers you want swapped "))
-  ones = digs%10
-  tens = digs //10
-  print (f"{ones}{tens}")
-twoDigs()
+digs = int(input("what numbers you want swapped "))
+ones = digs%10
+tens = digs //10
+print (f"{ones}{tens}")
 #Swap integers
